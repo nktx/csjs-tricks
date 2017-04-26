c223714e6805acc59158bc037dd2225e68655e2e
nktx <nkt.unknown@gmail.com>
2017-04-26 17:46:48 +0800

    add a browser shortcut to clean up the absence report

---
+++ cleanUpCHCReport.js
@@ -0,0 +1,21 @@
+javascript: (function() {
+  
+  // remove records that are not absenteeism
+  var tds = document.querySelectorAll("tr > td:nth-child(37)");
+  
+  for (var i = 0; i < tds.length; i++) {
+    if (tds[i].innerText === "") {
+      tds[i].parentElement.remove();
+    }
+  }
+
+  // remove class tables that don't have any record
+  var tables = document.querySelectorAll("table");
+
+  for (var j = 0; j < tables.length; j++) {
+    if (tables[j].children[0].children.length === 3) {
+      tables[j].previousSibling.previousSibling.previousSibling.previousSibling.remove();
+      tables[j].remove();
+    }
+  }
+})()
